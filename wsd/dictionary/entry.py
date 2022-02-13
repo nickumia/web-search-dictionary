47753565ac921c8a571fd2b7499e3e581012b673
nickumia <nickumia6@gmail.com>
2022-02-13 00:09:44 -0500

    new: added basic definition class
    Initial support will only be for basic features
     (pos and definition only\!)

---
+++ wsd/dictionary/entry.py
@@ -0,0 +1,36 @@
+
+
+class Word():
+    def __init__(self, word):
+        '''
+        IN: word, str: word to define
+        VAR: pronounciation, str: phonetic spelling of word
+        VAR: definition, dict(int: dict(str)): meanings of word
+            e.g. {0 : {'pos': 'noun',
+                       'definition': 'a clear explanation of word'},
+                  1 : {'pos': 'adjective',
+                       'definition': 'a descriptively delicious adjective'}
+        VAR: parts_of_speech, list: all known parts of speech the word can be
+                                    used in.
+        '''
+        self.id = word.lower()
+        self.pronounciation = None
+        self.definition = {}
+        self.parts_of_speech = []
+        self.no_of_senses = 0
+
+    def addPronounciation(self, pronounce):
+        self.pronounciation = pronounce
+
+    def addDefinition(self, new_definition):
+        try:
+            new_pos = new_definition['pos']
+            if new_pos not in self.parts_of_speech:
+                self.parts_of_speech.append(new_pos)
+        except KeyError:
+            pass
+        self.definition[self.no_of_senses] = new_definition
+        self.no_of_senses += 1
+
+    def getDefinitions(self):
+        return self.definition
