e20928e102552fa595f2258201fc435468d37f98
nickumia <nickumia6@gmail.com>
2022-02-13 19:01:20 -0500

    update: reorg parsing functions
    Fix bad results: 'People also ask', 'See results about', '·';

--- tests/test_wsd.py
+++ tests/test_wsd.py
@@ -4,5 +4,6 @@ import wsd
 
 def test_lookup():
     definitions = wsd.lookup('equal')
+    print(definitions.getDefinitions())
     assert definitions.getPronounciation() == '/&#712;&#275;kw&#601;l/'
-    assert len(definitions.getDefinitions()) == 10
+    assert len(definitions.getDefinitions()) == 8
--- wsd/web/parser.py
+++ wsd/web/parser.py
@@ -3,7 +3,22 @@ from lxml import etree
 import re
 
 
-def parseHTML(web_response):
+POS_TAGS = [
+    'noun',
+    'adjective',
+    'verb'
+]
+
+MISC = ['G', 'o', 'g', 'l', 'e', 'Videos', 'Please click', 'here', 'All',
+        'News', 'Images', 'Videos', 'Maps', 'Shopping', 'Books', '×',
+        'Search tools', 'Any time', 'Past hour', 'Past 24 hours', '',
+        'Past week', 'Past month', 'Past year', 'All results', 'Verbatim',
+        'Related searches', 'Next &gt;', '&nbsp;-&nbsp;', 'Learn more',
+        'Sign in', 'Settings', 'Privacy', 'Terms', 'People also ask',
+        'See results about', '·']
+
+
+def LXML_preprocessHTML(web_response):
     if type(web_response.content) == str:
         hdoc = etree.fromstring(web_response.content)
     else:
@@ -26,13 +41,31 @@ def parseHTML(web_response):
         content = re.sub(r'<script.*?</script>', '', content)
         content = re.sub(r'<!--.*?-->', '', content)
         content = re.sub(r'<meta .*?">', '', content)
+
+        # TODO: Fix timing of this replacement
+        content = re.sub(r'[0-9]+ days ago', '', content)
         # print(content)
         hdoc = etree.fromstring(content)
     return hdoc
 
 
-POS_TAGS = [
-    'noun',
-    'adjective',
-    'verb'
-]
+def LXML_parseHTML(parsed, target):
+    pronounciation = ""
+    definitions = []
+    current_pos = None
+
+    for e in parsed.iter():
+        if e.text is not None:
+            text_ = e.text.strip().replace('\xa0', '')
+            tag_ = e.tag.strip()
+            if '/&#' in text_:
+                pronounciation = text_
+            elif tag_ == 'span' and text_ in POS_TAGS:
+                current_pos = text_
+            else:
+                if target.lower() not in text_.lower() and text_ not in MISC:
+                    definitions.append({
+                        'pos': current_pos,
+                        'definition': text_
+                    })
+    return pronounciation, definitions
