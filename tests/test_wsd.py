4b88454896369614c1623dbdafe905d4869203f5
nickumia <nickumia6@gmail.com>
2022-02-13 19:42:17 -0500

    tests: add another test

--- tests/test_wsd.py
+++ tests/test_wsd.py
@@ -7,3 +7,16 @@ def test_lookup():
     print(definitions.getDefinitions())
     assert definitions.getPronounciation() == '/&#712;&#275;kw&#601;l/'
     assert len(definitions.getDefinitions()) == 8
+
+
+def test_example():
+    # Get the definitions for 'special'
+    entry = wsd.lookup('special')
+
+    # Get the pronounciation for 'special'
+    print(entry.getPronounciation())
+
+    # Get the definitions
+    for key, sense in entry.getDefinitions().items():
+        print('Part of speech [%d]: %s' % (key, sense['pos']))
+        print('Definition [%d]: %s' % (key, sense['definition']))
