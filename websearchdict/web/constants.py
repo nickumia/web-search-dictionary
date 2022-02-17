df5dff62fbb903a9e828b9265d3c2b798ef499bb
nickumia <nickumia6@gmail.com>
2022-02-16 21:30:34 -0500

    update: more nonsense

--- websearchdict/web/constants.py
+++ websearchdict/web/constants.py
@@ -37,8 +37,8 @@ BAD_PHRASES = [
     r'Definition of ([a-z]|[A-z])+(.*)?',
     r'How to pronounce ([a-z]|[A-z])+',
     r'Example (of )?([a-z]|[A-z])+( .*)?',
-    r'([a-zA-Z]+ \([a-zA-Z]+\))?((d|D)efinition |(and|&) |(m|M)eaning|'
-    r',| |(s|S)ynonyms?)+ \| .*',
+    r'([a-zA-Z])+( \([a-zA-Z]+\))?((d|D)efinition |(and|&) |(m|M)eaning|'
+    r',| |(s|S)ynonyms?|(e|E)xamples?)+ \| .*',
     r'(A Definition)? &amp; Meaning (-|\|) ',
     r'Meaning of ([a-z]|[A-z])+( .*)?',
     # Dates/Times/Money/Ratings
